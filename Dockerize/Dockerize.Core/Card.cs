5d6f3c933ce9156d1d274d45b99157480c7dba63
Wellington Antonio Zaneze <wellington.zaneze@outlook.com.br>
2019-02-06 21:51:23 -0200

    Add entities.

---
+++ Dockerize/Dockerize.Core/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dockerize.Core
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int CardTypeId { get; set; }
+        public CardType CardType { get; set; }
+    }
+}
---
+++ Dockerize/Dockerize.Core/CardType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dockerize.Core
+{
+    public class CardType
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
