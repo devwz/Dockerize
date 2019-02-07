df805d8fb2d0cc162686a4cbe3f928f5863ac8bf
Wellington Antonio Zaneze <wellington.zaneze@outlook.com.br>
2019-02-06 22:06:54 -0200

    Add EF Migrations.

--- Dockerize/Dockerize.Core/Card.cs
+++ Dockerize/Dockerize.Core/Card.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Dockerize.Core
@@ -7,7 +8,10 @@ namespace Dockerize.Core
     public class Card
     {
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
         public int CardTypeId { get; set; }
         public CardType CardType { get; set; }
     }
---
+++ Dockerize/Dockerize/Data/Migrations/20190206235823_InitialCreate.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dockerize.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CardTypeContext",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CardTypeContext", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CardContext",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: false),
+                    CardTypeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CardContext", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CardContext_CardTypeContext_CardTypeId",
+                        column: x => x.CardTypeId,
+                        principalTable: "CardTypeContext",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CardContext_CardTypeId",
+                table: "CardContext",
+                column: "CardTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CardContext");
+
+            migrationBuilder.DropTable(
+                name: "CardTypeContext");
+        }
+    }
+}
